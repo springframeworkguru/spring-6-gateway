839adfda4e5a8ac605332e5483f1f521b005b39a
John Thompson <john@springframework.guru>
2024-05-21 11:28:22 -0400

    Updating Spring Sec config

--- src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,12 +16,11 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SpringSecConfig {
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
-        http.csrf().disable()
-                .authorizeExchange()
-                .anyExchange().authenticated()
-                .and()
-                .oauth2ResourceServer().jwt();
 
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+        
         return http.build();
     }
 }
